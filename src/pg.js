11d9ec857a5e2ad332e224b0aa1f6f1a640704f3
Alexander Ward <alexanderward5@gmail.com>
2021-06-29 17:19:03 -0600

    configure dotenv for db credentials

---
+++ src/pg.js
@@ -0,0 +1,21 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: process.env.dbhost,
+  port: process.env.dbport,
+  user: process.env.dbuser,
+  password: process.env.dbpassword,
+  max: 20,
+  idleTimeoutMillis: 15000,
+  connectionTimeoutMillis: 2000,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error("connection error", err.stack);
+  } else {
+    console.log("connected");
+  }
+});
+
+module.exports = pool;
