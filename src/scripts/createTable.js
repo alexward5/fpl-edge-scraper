5da9c5851bd8d97220d2403447356be34e7c5266
Alexander Ward <alexanderward5@gmail.com>
2021-06-30 15:59:00 -0600

    formatting

--- src/scripts/createTable.js
+++ src/scripts/createTable.js
@@ -4,17 +4,17 @@ async function createTable(schema, tableName, columnTemplate) {
   try {
     const res = await pool.query(
       `
-    CREATE TABLE IF NOT EXISTS "${schema}".${tableName} (
-      ${columnTemplate.columns
-        .map(
-          (column) =>
-            `${column.column_name} ${column.column_type} ${
-              column.primary_key ? "PRIMARY KEY" : ""
-            }`
-        )
-        .join()}
-    );
-  `
+        CREATE TABLE IF NOT EXISTS "${schema}".${tableName} (
+          ${columnTemplate.columns
+            .map(
+              (column) =>
+                `${column.column_name} ${column.column_type} ${
+                  column.primary_key ? "PRIMARY KEY" : ""
+                }`
+            )
+            .join()}
+        );
+      `
     );
 
     console.log(`Successfully created "${schema}".${tableName}`);
--- src/scripts/seedTable.js
+++ src/scripts/seedTable.js
@@ -4,17 +4,17 @@ async function seedTable(schema, tableName, columnTemplate, jsonString) {
   try {
     const res = await pool.query(
       `
-      INSERT INTO "${schema}".${tableName}(
-        ${columnTemplate.columns.map((column) => column.column_name).join()}
-      )
-      SELECT *
-      FROM json_to_recordset('${jsonString}') AS x(
-        ${columnTemplate.columns
-          .map((column) => `${column.column_name} ${column.column_type}`)
-          .join()}
-      )
-      ON CONFLICT DO NOTHING
-  `
+        INSERT INTO "${schema}".${tableName}(
+          ${columnTemplate.columns.map((column) => column.column_name).join()}
+        )
+        SELECT *
+        FROM json_to_recordset('${jsonString}') AS x(
+          ${columnTemplate.columns
+            .map((column) => `${column.column_name} ${column.column_type}`)
+            .join()}
+        )
+        ON CONFLICT DO NOTHING
+      `
     );
 
     console.log(`Successfully seeded "${schema}".${tableName}`);
