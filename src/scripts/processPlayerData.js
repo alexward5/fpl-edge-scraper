e1c204feb75a1c5acf21c8bc705ba5ee001eac18
alexward5 <alexanderward5@gmail.com>
2021-08-11 17:55:58 -0600

    move teamMetadata parsing to parent function

--- src/scripts/processPlayerData.js
+++ src/scripts/processPlayerData.js
@@ -3,10 +3,14 @@ const processPlayerMetadata = require("./processPlayerMetadata");
 const processPlayerSeasonStats = require("./processPlayerSeasonStats");
 const processGwStats = require("./processGwStats");
 const processTeamMetadata = require("./processTeamMetadata");
+const processTeamUnderstatData = require("./processTeamUnderstatData");
 
 async function processPlayerData(season) {
   const playerMetadata = await csvToJSON(
     `./Fantasy-Premier-League/data/${season}/player_idlist.csv`
+  );
+  const teamMetadata = await csvToJSON(
+    `./Fantasy-Premier-League/data/${season}/teams.csv`
   );
 
   try {
@@ -14,7 +18,8 @@ async function processPlayerData(season) {
     await processPlayerMetadata(playerMetadata, season);
     await processPlayerSeasonStats(playerMetadata, season);
     await processGwStats(season);
-    await processTeamMetadata(season);
+    await processTeamMetadata(teamMetadata, season);
+    await processTeamUnderstatData(teamMetadata, season);
   } catch (err) {
     console.error(err);
   } finally {
--- src/scripts/processTeamMetadata.js
+++ src/scripts/processTeamMetadata.js
@@ -1,14 +1,9 @@
 const createTable = require("./dbFunctions/createTable");
 const seedTable = require("./dbFunctions/seedTable");
-const csvToJSON = require("../helpers/csvToJSON");
 const dbTableTemplates = require("../templates/dbtables.json");
 
-async function processTeamMetadata(season) {
+async function processTeamMetadata(teamMetadata, season) {
   await createTable(season, "team_metadata", dbTableTemplates.team_metadata);
-
-  const teamMetadata = await csvToJSON(
-    `./Fantasy-Premier-League/data/${season}/teams.csv`
-  );
 
   const teamMetadataString = JSON.stringify(teamMetadata);
 
