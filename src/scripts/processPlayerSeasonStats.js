fe2e9037b6bb688381d5027d2ec821f9fece11b3
alexward5 <alexanderward5@gmail.com>
2021-08-24 09:27:02 -0600

    cover edge case where two players share the same name

--- src/scripts/processPlayerSeasonStats.js
+++ src/scripts/processPlayerSeasonStats.js
@@ -11,19 +11,34 @@ async function processPlayerSeasonStats(playerMetadata, season) {
     `./Fantasy-Premier-League/data/${season}/cleaned_players.csv`
   );
 
+  // This map is used to ensure that players who share the same first and last name will each be given a unique id
+  const usedIdsMap = {};
+
   // Player id is missing in season stats CSV, so find it in metadata and add it to season stats object
   const playerSeasonStatsWithIds = playerSeasonStats.map((playerStatsObj) => {
-    const foundPlayer = playerMetadata.find(
+    const foundPlayers = playerMetadata.filter(
       (playerMetadataObj) =>
         playerMetadataObj.first_name === playerStatsObj.first_name &&
         playerMetadataObj.second_name === playerStatsObj.second_name
     );
-    if (!foundPlayer) {
+    if (!foundPlayers.length) {
       throw new Error(
-        `Error processing season stats for ${playerStatsObj.first_name} ${playerStatsObj.second_name}`
+        `Error processing season stats for ${playerStatsObj.first_name} ${playerStatsObj.second_name} - unable to find player name match in metadata`
       );
+      // If only one player with the first/last name is found, then return the id from our metadata with that player's season stats
+    } else if (foundPlayers.length === 1) {
+      return { ...playerStatsObj, id: foundPlayers[0].id };
+    } else {
+      // If more than one player with the first/last name is found, iterate over those players until an unused id is found
+      for (let i = 0; i < foundPlayers.length; i += 1) {
+        if (!usedIdsMap[foundPlayers[i].id]) {
+          // When an unused id is found, add that id to our map so it will not be used again
+          usedIdsMap[foundPlayers[i].id] = true;
+          return { ...playerStatsObj, id: foundPlayers[i].id };
+        }
+      }
     }
-    return { ...playerStatsObj, id: foundPlayer.id };
+    return null;
   });
 
   const playerSeasonStatsString = escapeQuotes(playerSeasonStatsWithIds);
