0114eafb121dd4f49c190cdf747ddaaecb90543e
Alexander Ward <alexanderward5@gmail.com>
2022-04-03 19:35:38 -0600

    refactor/cleanup

--- src/api-scripts/processAPIData.js
+++ src/api-scripts/processAPIData.js
@@ -1,76 +1,10 @@
-const fetch = require("node-fetch");
-const JSSoup = require("jssoup").default;
-const teamNameMapping = require("../mappings/team_names.json");
-
-async function getFplData() {
-    // Get player data from FPL API
-    const response = await fetch(
-        "https://fantasy.premierleague.com/api/bootstrap-static/"
-    );
-    const body = await response.json();
-    const fplTeamData = body.teams;
-    const fplPlayerData = body.elements;
-
-    return {
-        fplTeamData,
-        fplPlayerData,
-    };
-}
-
-async function getFBRefTeamUrls() {
-    // Scrape fb-ref team URLs
-    const res = await fetch(
-        "https://fbref.com/en/comps/9/Premier-League-Stats"
-    );
-    const rawHTML = await res.text();
-    const soup = new JSSoup(rawHTML);
-
-    const teamURLs = [];
-
-    const tableRows = soup.find("table").find("tbody").findAll("tr");
-
-    tableRows.forEach((teamRow) => {
-        const dataCols = teamRow.findAll("td");
-        dataCols.forEach((dataCol) => {
-            const dataStat = dataCol.attrs["data-stat"];
-            if (dataStat === "squad") {
-                teamURLs.push(
-                    `https://fbref.com${dataCol.find("a").attrs.href}`
-                );
-            }
-        });
-    });
-
-    return teamURLs;
-}
+const FBRefTable = require("../classes/FBRefTable");
 
 async function processAPIData() {
     try {
-        const { fplPlayerData, fplTeamData } = await getFplData();
-        console.log(fplPlayerData.length);
-        const fbRefTeamURLs = await getFBRefTeamUrls();
-
-        // Find the FBRef URL for each team
-        const teamData = fplTeamData.map((fplTeamDataObj) => {
-            const fplTeamName = fplTeamDataObj.name;
-            const teamNameMapObj = teamNameMapping.find(
-                (teamNameObj) => teamNameObj.fplName === fplTeamName
-            );
-            const teamURL = fbRefTeamURLs.find((url) =>
-                url.includes(teamNameMapObj.fbRefURLName)
-            );
-            if (!teamURL) {
-                throw new Error(
-                    `Error: No FBRef team URL match found for ${fplTeamDataObj.name}`
-                );
-            }
-            return {
-                ...fplTeamDataObj,
-                fb_ref_url: teamURL,
-            };
-        });
-
-        console.log(teamData);
+        const table = new FBRefTable("https://fbref.com/en/comps/9/Premier-League-Stats", 0);
+        const tableJSON = await table.getTableJSON();
+        console.log(tableJSON);
     } catch (err) {
         console.error(err);
     } finally {
