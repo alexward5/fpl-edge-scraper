256690e8678fb86a5375725c5566f6da207c0d68
Alexander Ward <alexanderward5@gmail.com>
2022-04-03 19:36:27 -0600

    finish fbreftable class poc

--- src/classes/FBRefTable.js
+++ src/classes/FBRefTable.js
@@ -2,40 +2,58 @@ const fetch = require("node-fetch");
 const JSSoup = require("jssoup").default;
 
 class FBRefTable {
-    constructor(tableUrl, tableIndex) {
-        this.tableUrl = tableUrl;
+    constructor(tableURL, tableIndex) {
+        this.tableURL = tableURL;
         this.tableIndex = tableIndex;
-        this.tableJSON = this.getTableJSON();
     }
 
     async getTableJSON() {
-        const res = await fetch(
-            "https://fbref.com/en/squads/b8fd03ef/2020-2021/Manchester-City-Stats"
-        );
-        const rawHTML = await res.text();
-        const soup = new JSSoup(rawHTML);
-
-        const table = soup.findAll("table")[this.tableIndex];
-        const tableRows = table.find("tbody").findAll("tr");
-
         const tableRowsJSON = [];
 
-        tableRows.forEach((teamRow) => {
-            const headCell = teamRow.find("th");
-            const headCellDataStat = headCell.attrs["data-stat"];
-            const headCellDataLink = headCell.find("a");
-            if (headCellDataLink) {
-                tableRowsJSON.push({
-                    [headCellDataStat]: headCellDataLink.text,
-                });
-            } else {
-                tableRowsJSON.push({
-                    headCellDataStat: headCell.text,
-                });
-            }
-        });
+        try {
+            const res = await fetch(this.tableURL);
+            const rawHTML = await res.text();
+            const soup = new JSSoup(rawHTML);
 
-        console.log(tableRowsJSON);
+            const table = soup.findAll("table")[this.tableIndex];
+            const tableRows = table.find("tbody").findAll("tr");
+
+            tableRows.forEach((tableRow) => {
+                const rowJSON = {};
+
+                // Add data from head cell to rowJSON
+                const headCell = tableRow.find("th");
+                const headCellDataStat = headCell.attrs["data-stat"];
+                const headCellDataLink = headCell.find("a");
+                if (headCellDataLink) {
+                    rowJSON[headCellDataStat] = headCellDataLink.text;
+                    rowJSON[`${headCellDataStat}_url`] = `https://fbref.com${headCellDataLink.attrs.href}`;
+                } else {
+                    rowJSON[headCellDataStat] = headCell.text;
+                }
+
+                // Add data from data cells to rowJSON
+                const tableCells = tableRow.findAll("td");
+                tableCells.forEach((tableCell) => {
+                    const tableCellDataStat = tableCell.attrs["data-stat"];
+                    const tableCellDataLink = tableCell.find("a");
+                    if (tableCellDataLink) {
+                        rowJSON[tableCellDataStat] = tableCellDataLink.text;
+                        rowJSON[`${tableCellDataStat}_url`] = `https://fbref.com${tableCellDataLink.attrs.href}`;
+                    } else {
+                        rowJSON[tableCellDataStat] = tableCell.text;
+                    }
+                });
+
+                tableRowsJSON.push(rowJSON);
+            });
+        } catch (err) {
+            console.error(`Error creating JSON from table ${this.tableURL}[${this.tableIndex}]: ${err.message}`);
+        } finally {
+            console.log(`Finished creating JSON from table ${this.tableURL}[${this.tableIndex}]`);
+        }
+
+        return tableRowsJSON;
     }
 }
 
