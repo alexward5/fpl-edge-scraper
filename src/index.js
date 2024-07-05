ddba28a25946d9feef50ab27ffdd5fa5856fd1bf
Alex Ward <alexanderward5@gmail.com>
2024-07-04 20:12:52 -0600

    add bools to toggle api/csv runs

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
-// require("dotenv").config();
 import "dotenv/config";
-// const fs = require("fs");
-// const shell = require("shelljs");
-// const processCSVData = require("./csv-scripts/processCSVData");
-// const processAPIData = require("./api-scripts/processAPIData");
-import processCSVData from "./csv-scripts/processCSVData.cjs";
-import processAPIData from "./api-scripts/processAPIData.js";
 import fs from "fs";
 import shell from "shelljs";
+import processCSVData from "./csv-scripts/processCSVData.cjs";
+import processAPIData from "./api-scripts/processAPIData.js";
+
+const PROCESS_CSV = false;
+const PROCESS_API = true;
 
 // Set Permier League season, determines which CSV file data will be pulled from
 const season = "2019-20";
 
 (() => {
-    // Check if FPL data already exists locally, if not then pull the repo
-    if (!fs.existsSync("./Fantasy-Premier-League")) {
-        shell.exec("git clone https://github.com/vaastav/Fantasy-Premier-League", { async: true }, () => {
+    if (PROCESS_CSV) {
+        // Check if FPL data already exists locally, if not then pull the repo
+        if (!fs.existsSync("./Fantasy-Premier-League")) {
+            shell.exec("git clone https://github.com/vaastav/Fantasy-Premier-League", { async: true }, () => {
+                processCSVData(season);
+            });
+        } else {
             processCSVData(season);
-        });
-    } else {
-        processCSVData(season);
+        }
     }
 })();
 
-// (() => {
-//     processAPIData();
-// })();
+(() => {
+    if (PROCESS_API) {
+        processAPIData();
+    }
+})();
